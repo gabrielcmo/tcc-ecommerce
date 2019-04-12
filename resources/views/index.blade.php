8890bdaf8a4c43fa443f4e86426dba56abbd5475
GeovannePad <geovannepadilha@hotmail.com>
2019-04-12 14:44:28 -0300

    Ajeitando cronômetro

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -119,8 +119,8 @@
   <!-- Time Counter -->
   <script type="text/javascript">
     // Set the date we're counting down to
-    var countDownDate = new Date();
-    countDownDate.setDate(countDownDate.getDate() + 120);
+    var countDownDate = new Date(2019,11,01);
+    countDownDate.setDate(countDownDate.getDate());
 
     // Update the count down every 1 second
     var x = setInterval(function () {
