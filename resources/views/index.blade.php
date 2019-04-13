9a2e2175f8406be27f4e0430d811e89866781c92
Geovanne Padilha <geovannepadilha@hotmail.com>
2019-04-13 16:23:08 -0300

    Ajeitando a view default e deletando views partials e começando a home page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,170 +1,191 @@
-@extends('layouts.index')
+@extends('layouts/default')
 
-@section('carousel')
-<!--Carousel Wrapper-->
-<div id="carousel-example-1z" class="carousel slide carousel-fade pt-4" data-ride="carousel">
+@section('title', 'Home')
 
-<!--Indicators-->
-<ol class="carousel-indicators">
-  <li data-target="#carousel-example-1z" data-slide-to="0" class="active"></li>
-  <li data-target="#carousel-example-1z" data-slide-to="1"></li>
-</ol>
-<!--/.Indicators-->
-
-<!--Slides-->
-<div class="carousel-inner" role="listbox">
-
-  <!--First slide-->
-  <div class="carousel-item active">
-    <div class="view" style="background-repeat: no-repeat; background-size: cover;">
-      <img src="img/carousel/carousel-1.jpg" alt="">
-
-      <!-- Mask & flexbox options-->
-      <div class="mask rgba-black-strong d-flex justify-content-center align-items-center">
-
-        <!-- Content -->
-        <div class="text-center white-text mx-5 wow fadeIn">
-          <h1 class="mb-4">
-            <strong>Os melhores produtos para decorar sua casa</strong>
-          </h1>
-
-          <p>
-            <strong>Preço bom e satisfação 100%</strong>
-          </p>
-
-          <p class="mb-4 d-none d-md-block">
-            <strong>Com uma imensa variedade de escolha, você pode se sentir a vontade para comprar o que quiser, pelo melhor preço!</strong>
-          </p>
-
-          <a target="_blank" href="products/3" class="btn btn-outline-white btn-lg">Ver decorações
-            <i class="fas fa-graduation-cap ml-2"></i>
-          </a>
-        </div>
-        <!-- Content -->
-
-      </div>
-      <!-- Mask & flexbox options-->
-
-    </div>
-  </div>
-  <!--/First slide-->
-
-  <!--Second slide-->
-  <div class="carousel-item">
-    <div class="view" style="background-repeat: no-repeat; background-size: cover;">
-      <img src="img/carousel/carousel-2.jpg" alt="">
-
-      <!-- Mask & flexbox options-->
-      <div class="mask rgba-black-strong d-flex justify-content-center align-items-center">
-
-        <!-- Content -->
-        <div class="text-center white-text mx-5 wow fadeIn">
-          <h1 class="mb-4">
-            <strong>Toalhas</strong>
-          </h1>
-
-          <p>
-            <strong>Leve o que quiser, a hora é agora!</strong>
-          </p>
-
-          <p class="mb-4 d-none d-md-block">
-            <strong>Não perca tempo, tome banho e se seque com a toalhas mais macias e mais baratas do mercado</strong>
-          </p>
-
-          <a target="_blank" href="products/5" class="btn btn-outline-white btn-lg">Ver toalhas
-            <i class="fas fa-graduation-cap ml-2"></i>
-          </a>
-        </div>
-        <!-- Content -->
-
-      </div>
-      <!-- Mask & flexbox options-->
-
-    </div>
-  </div>
-  <!--/Second slide-->
-</div>
-<!--/.Slides-->
-
-<!--Controls-->
-<a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
-  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-  <span class="sr-only">Previous</span>
-</a>
-<a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
-  <span class="carousel-control-next-icon" aria-hidden="true"></span>
-  <span class="sr-only">Next</span>
-</a>
-<!--/.Controls-->
-
-</div>
-<!--/.Carousel Wrapper-->
-@endsection
-
-@section('categories')
-    @foreach($categories as $category)
-        <li class="nav-item">
-            <a class="nav-link" href="products/{{ $category->id }}">{{ $category->name }}</a>
-        </li>
-    @endforeach
-@endsection
-
-@section('products')
-  <?php $qtd = 0;
-  foreach($products as $product){
-    if($qtd == 20){
-      $id_product = $product->id; 
-      break;
+@section('stylesheets')
+  <link rel="stylesheet" href="css/styleHome.css">
+  <style type="text/css">
+    html,
+    body,
+    header,
+    .carousel {
+      height: 60vh;
     }
-      
-  $qtd++; ?>
-    <!--Grid column-->
-    <div class="col-lg-3 col-md-6 mb-4 d-flex align-content-around flex-wrap">
 
-      <!--Card-->
-      <div class="card">
+    @media (max-width: 740px) {
 
-      <!--Card image-->
-      <div class="view overlay" style="height:300px;">
-          <img src="img/products/placeholder-1.jpg" class="card-img-top" alt="">
-          <a>
-          <div class="mask rgba-white-slight"></div>
-          </a>
-      </div>
-      <!--Card image-->
+      html,
+      body,
+      header,
+      .carousel {
+        height: 100vh;
+      }
+    }
 
-      <!--Card content-->
-      <div class="card-body text-center" style="height:400px;">
-          <!--Category & Title-->
-          <a href="" class="grey-text">
-          <h5>{{ $product->name }}</h5>
-          </a>
-          <h5>
-          <strong>
-              <a href="" class="dark-grey-text">R${{$product->value}}</a>
-          </strong>
-          </h5>
+    @media (min-width: 800px) and (max-width: 850px) {
 
-          <h4 class="font-weight-bold blue-text">
-          <strong>{{$product->description}}</strong>
-          </h4>
+      html,
+      body,
+      header,
+      .carousel {
+        height: 100vh;
+      }
+    }
 
-      </div>
-      <!--Card content-->
-
-      </div>
-      <!--Card-->
-    </div>
-    <!--Grid column-->
-  <?php } ?>
+  </style>
 @endsection
 
-@section('pagination')
-  <?php $number_pg = 2;?>
-  @for($i = 0; $i <= 5; $i++)
-    <li class="page-item">
-      <a class="page-link" href="{{redirect('$number_pg?id_last_product=$id_last_product')}}">{{$number_pg}}</a>
-    </li>
-    <?php $number_pg++; ?>
-  @endfor
+@section('other-contents')
+  <!--Carousel Wrapper-->
+  <div id="carousel-example-1z" class="carousel slide carousel-fade pt-4" data-ride="carousel">
+
+      <!--Indicators-->
+      <ol class="carousel-indicators">
+        <li data-target="#carousel-example-1z" data-slide-to="0" class="active"></li>
+        <li data-target="#carousel-example-1z" data-slide-to="1"></li>
+        <li data-target="#carousel-example-1z" data-slide-to="2"></li>
+      </ol>
+      <!--/.Indicators-->
+  
+      <!--Slides-->
+      <div class="carousel-inner" role="listbox">
+  
+        <!--First slide-->
+        <div class="carousel-item active">
+          <div class="view" style="background-image: url('https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/8-col/img%282%29.jpg'); background-repeat: no-repeat; background-size: cover;">
+  
+            <!-- Mask & flexbox options-->
+            <div class="mask rgba-black-strong d-flex justify-content-center align-items-center">
+  
+              <!-- Content -->
+              <div class="text-center white-text mx-5 wow fadeIn">
+                <h1 class="mb-4">
+                  <strong>Learn Bootstrap 4 with MDB</strong>
+                </h1>
+  
+                <p>
+                  <strong>Best & free guide of responsive web design</strong>
+                </p>
+  
+                <p class="mb-4 d-none d-md-block">
+                  <strong>The most comprehensive tutorial for the Bootstrap 4. Loved by over 500 000 users. Video and
+                    written versions
+                    available. Create your own, stunning website.</strong>
+                </p>
+  
+                <a target="_blank" href="https://mdbootstrap.com/education/bootstrap/" class="btn btn-outline-white btn-lg">Start
+                  free tutorial
+                  <i class="fas fa-graduation-cap ml-2"></i>
+                </a>
+              </div>
+              <!-- Content -->
+  
+            </div>
+            <!-- Mask & flexbox options-->
+  
+          </div>
+        </div>
+        <!--/First slide-->
+  
+        <!--Second slide-->
+        <div class="carousel-item">
+          <div class="view" style="background-image: url('https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/8-col/img%283%29.jpg'); background-repeat: no-repeat; background-size: cover;">
+  
+            <!-- Mask & flexbox options-->
+            <div class="mask rgba-black-strong d-flex justify-content-center align-items-center">
+  
+              <!-- Content -->
+              <div class="text-center white-text mx-5 wow fadeIn">
+                <h1 class="mb-4">
+                  <strong>Learn Bootstrap 4 with MDB</strong>
+                </h1>
+  
+                <p>
+                  <strong>Best & free guide of responsive web design</strong>
+                </p>
+  
+                <p class="mb-4 d-none d-md-block">
+                  <strong>The most comprehensive tutorial for the Bootstrap 4. Loved by over 500 000 users. Video and
+                    written versions
+                    available. Create your own, stunning website.</strong>
+                </p>
+  
+                <a target="_blank" href="https://mdbootstrap.com/education/bootstrap/" class="btn btn-outline-white btn-lg">Start
+                  free tutorial
+                  <i class="fas fa-graduation-cap ml-2"></i>
+                </a>
+              </div>
+              <!-- Content -->
+  
+            </div>
+            <!-- Mask & flexbox options-->
+  
+          </div>
+        </div>
+        <!--/Second slide-->
+  
+        <!--Third slide-->
+        <div class="carousel-item">
+          <div class="view" style="background-image: url('https://mdbootstrap.com/img/Photos/Horizontal/E-commerce/8-col/img%285%29.jpg'); background-repeat: no-repeat; background-size: cover;">
+  
+            <!-- Mask & flexbox options-->
+            <div class="mask rgba-black-strong d-flex justify-content-center align-items-center">
+  
+              <!-- Content -->
+              <div class="text-center white-text mx-5 wow fadeIn">
+                <h1 class="mb-4">
+                  <strong>Learn Bootstrap 4 with MDB</strong>
+                </h1>
+  
+                <p>
+                  <strong>Best & free guide of responsive web design</strong>
+                </p>
+  
+                <p class="mb-4 d-none d-md-block">
+                  <strong>The most comprehensive tutorial for the Bootstrap 4. Loved by over 500 000 users. Video and
+                    written versions
+                    available. Create your own, stunning website.</strong>
+                </p>
+  
+                <a target="_blank" href="https://mdbootstrap.com/education/bootstrap/" class="btn btn-outline-white btn-lg">Start
+                  free tutorial
+                  <i class="fas fa-graduation-cap ml-2"></i>
+                </a>
+              </div>
+              <!-- Content -->
+  
+            </div>
+            <!-- Mask & flexbox options-->
+  
+          </div>
+        </div>
+        <!--/Third slide-->
+  
+      </div>
+      <!--/.Slides-->
+  
+      <!--Controls-->
+      <a class="carousel-control-prev" href="#carousel-example-1z" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carousel-example-1z" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+      <!--/.Controls-->
+  
+    </div>
+    <!--/.Carousel Wrapper-->
+  
+@endsection
+
+@section('content')
+  <div class="container-flex">
+    
+  </div>
+@endsection
+
+@section('scripts')
+  
 @endsection
---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>@yield('title')</title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="css/mdb.min.css" rel="stylesheet">
+  <!-- Your custom styles (optional) -->
+  <link href="css/style.min.css" rel="stylesheet">
+  
+  @yield('stylesheets')
+
+</head>
+<body>
+    <!-- Navbar -->
+    <nav class="navbar fixed-top navbar-expand-lg navbar-light white scrolling-navbar">
+      <div class="container">
+  
+        <!-- Brand -->
+        <a class="navbar-brand waves-effect" href="#" target="_blank">
+          <strong class="blue-text">Doomus</strong>
+        </a>
+  
+        <!-- Collapse -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+  
+        <!-- Links -->
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  
+          <!-- Left -->
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link waves-effect" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link waves-effect" href="#" target="_blank">About MDB</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link waves-effect" href="#" target="_blank">Free download</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link waves-effect" href="#" target="_blank">Free tutorials</a>
+            </li>
+          </ul>
+  
+          <!-- Right -->
+          <ul class="navbar-nav nav-flex-icons">
+            <li class="nav-item">
+              <a class="nav-link waves-effect">
+                <i class="fas fa-shopping-cart"></i>
+                <span class="clearfix d-none d-sm-inline-block"> Cart </span>
+              </a>
+            </li>   
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink-4" data-toggle="dropdown"
+                aria-haspopup="true" aria-expanded="false">
+                <i class="fas fa-user"></i></a>
+              <div class="dropdown-menu dropdown-menu-right dropdown-info" aria-labelledby="navbarDropdownMenuLink-4">
+                <a class="dropdown-item" href="#">Logar</a>
+                <a class="dropdown-item" href="#">Registrar</a>
+              </div>
+            </li>        
+          </ul>
+  
+        </div>
+  
+      </div>
+    </nav>
+    <!-- /Navbar -->
+
+  @yield('other-contents')
+
+  <!-- Main layout -->
+  <main>
+
+    @yield('content')
+
+  </main>
+  <!-- /Main layout -->
+
+  <!-- Footer -->
+  <footer>
+    
+  </footer>
+  <!-- /Footer -->
+
+  <!-- SCRIPTS -->
+
+  <!-- JQuery -->
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="js/popper.min.js"></script>
+
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="js/mdb.min.js"></script>
+
+  @yield('scripts')
+
+  <!-- Initializations -->
+  <script type="text/javascript">
+    // Animations initialization
+    new WOW().init();
+    @yield('animations')
+  </script>
+</body>
+</html>
