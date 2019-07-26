3ccc0718a8b487e1f0c60dbee19b9e6a7b8f03b9
gabriel <gabrielcarlosmoliveira@gmail.com>
2019-07-26 13:49:04 -0300

    index de teste arrumada - card

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,27 +8,25 @@
 
 @section('content')
   <div class="container">
-    <div class="row">
+  <div class="row">
       @foreach($products as $product)
-        <div class="row align-items-center">
-          <div class="col-lg-4 mt-1">
-            <div class="mdc-card">
-              <div class="mdc-card__primary-action">
-                @foreach ($product->image as $image)
-                  <img class="mdc-card__media mdc-card__media--16-9 mdc-card__media--square" src="{{asset("img/products/$image->filename")}}">   
-                @endforeach
-                  <div class="mdc-card__media-content" style="display: flex; align-items: flex-end;">
-                    <h2 class="mdc-typography mdc-typography--headline6" style="color: white; padding-left: 0.5rem">{{$product->name}}</h2>
-                  </div>
+        <?php $images = $product->image; ?>
+        <div class="col-md-4">
+          <div class="card">
+            @foreach($product->image as $image)
+              @if(isset($image) && $image !== null && $image->filename !== null || $image->filename !== '')
+                <div class="card-image">
+                  <img src="/img/products/{{$image->filename}}" alt="Produto" class="img-fluid">
                 </div>
-              </div>
-              <div class="mdc-card__actions">
-                <button class="mdc-icon-button material-icons mdc-card__action mdc-card_action--button" aria-label="Add to cart">
-                  add_shopping_cart
-                </button>
-              </div>
-            </div>
-          </div> 
+              @elseif(!isset($image))  
+                <div class="card-image">
+                  <img src="/img/doomus.png" alt="Produto" class="img-fluid">
+                </div>
+              @endif
+            @endforeach
+            <h3 class="card-title">{{ $product->name }}</h3>
+            <a class="btn btn-success" href="/carrinho/{{ $product->id }}/add">Adicionar ao carrinho</a><br>      
+          </div>
         </div>
       @endforeach
     </div>
