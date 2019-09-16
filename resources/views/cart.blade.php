cf6f53fc448ad93b813f15ed4ef3bed637e5f17b
Geovanne Padilha <geovannepadilha@hotmail.com>
2019-09-16 08:58:36 -0300

    Resumo do carrinho

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,8 +1,10 @@
 @extends('layouts.new_layout')
 
 @section('content')
-  <div class="row">
-    <div class="col-lg-9">
+<div class="">
+  <div class="row justify-content-center mt-3">
+    <div class="col-lg-7">
+      <h4>Seu carrinho</h4>
       <table class="table table-borderless">
         <thead>
           <tr>
@@ -22,12 +24,12 @@
                 </div>
               </div>
             </td>
-            <td class="w-25">
-              <input type="number" class="form-control w-50 inputQty" min="1" value="1" data-product="{{$loop->iteration}}">
+            <td class="w-25 align-middle">
+              <input type="number" class="form-control inputQty" style="width:4.5rem" min="1" value="1" data-product="{{$loop->iteration}}">
               <a class="text-center" href="#">Remover</a>
               <span class="d-none {{"productValue$loop->iteration"}}">R${{$item->price}}</span>
             </td>
-            <td class="{{"newProductValue$loop->iteration"}} w-25">
+            <td class="{{"newProductValue$loop->iteration"}} w-25 align-middle">
               R${{$item->price}}
             </td>
           </tr>
@@ -35,7 +37,36 @@
         </tbody>
       </table>
     </div>
+    <div class="col-lg-2">
+      <h4 class="d-flex justify-content-between align-items-center">
+        <span class="">Resumo do pedido</span>
+        <span class="badge badge-secondary">{{Cart::count()}}</span>
+      </h4>
+      <table class="table table-borderless">
+        <tbody>
+          <tr>
+            <th>Subtotal</th>
+            <td>{{}}</td>
+          </tr>
+          <tr>
+            <th>Frete</th>
+            <td></td>
+          </tr>
+          <tr class="border-top border-dark">
+            <th class="align-middle">Total</th>
+            <td>
+              <span class="d-flex">R${{Cart::total()}}</span>
+              <span class="d-flex text-success">10x de R${{Cart::total()/10}} s/juros no cartão</span>
+            </td>
+          </tr>
+          <tr>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
+</div>
 @endsection
 
 @section('scripts')
