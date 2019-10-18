45753157e83501628dc5ecb1387854823c0c386b
Geovanne Padilha <geovannepadilha@hotmail.com>
2019-10-18 15:28:10 -0300

    Voltando rotas ao normal

--- routes/web.php
+++ routes/web.php
@@ -11,267 +11,130 @@
 |
 */
 
-use Illuminate\Support\Facades\App;
+/*
+*   Cart routes
+*/
+Route::get('/carrinho/{product_id}/add/', 'CartController@addToCart');
+Route::get('/carrinho/add/', 'CartController@addToCart')->name('cart.add');
+Route::get('/carrinho/delete', 'CartController@clearCart')->name('cart.clear');
+Route::get('/carrinho', 'CartController@show')->name('user.cart');
+Route::get('/carrinho/{product_id}/remove', 'CartController@removeFromCart');
+Route::get('/carrinho/{product_rowId}/{qty}/{product_id}', 'CartController@changeQuantity');
 
-if (App::environment('local')) {
+/* 
+*   Autentificação - Login - Rede Social
+*/
+Auth::routes();
+Route::get('/auth/{provider}', 'Auth\LoginController@redirectToProvider')->name('loginSocial');
+Route::get('/auth/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
+
+/* 
+*   Pág. inicial 
+*/
+Route::get('/', 'IndexController@index')->name('landing');
+
+/* 
+*   Barra de pesquisa de produtos
+*/
+Route::get('/find', 'SearchController@find')->name('search');
+
+/*
+*   View produto
+*/
+Route::get('/produto/{id}', 'ProductController@show');
+
+Route::get('/ofertas', 'OfertasController@view')->name('offers');
+
+/*
+*   Checar CEP
+*/
+Route::get('/checkout/address/cep', 'CheckoutController@checkCep')->name('checkCep');
+
+Route::get('/cupom/validate', 'AdminController@cupomValidate');
+
+/*
+*   Calcular CEP
+*/
+Route::post('/calc/frete', 'CheckoutController@calcFrete')->name('calcFrete');
+
+Route::group(['middleware' => ['auth']], function (){
+    /*
+    *   User views
+    */
+    Route::get('/perfil', 'UserController@showProfile')->name('perfil');
+    Route::post('/perfil/update', 'UserController@updateProfile');
+    Route::get('/pedidos', 'OrderController@show')->name('orders');
+    Route::get('/historico', 'HistoricController@show')->name('historic');
+
+    Route::post('/avaliar', 'UserController@avaliate')->name('avaliate');
 
     /*
-    *   Cart routes
+    *   Limpar histórico
     */
-    Route::get('/carrinho/{product_id}/add/', 'CartController@addToCart');
-    Route::get('/carrinho/add/', 'CartController@addToCart')->name('cart.add');
-    Route::get('/carrinho/delete', 'CartController@clearCart')->name('cart.clear');
-    Route::get('/carrinho', 'CartController@show')->name('user.cart');
-    Route::get('/carrinho/{product_id}/remove', 'CartController@removeFromCart');
-    Route::get('/carrinho/{product_rowId}/{qty}/{product_id}', 'CartController@changeQuantity');
-
-    /* 
-    *   Autentificação - Login - Rede Social
-    */
-    Auth::routes();
-    Route::get('/auth/{provider}', 'Auth\LoginController@redirectToProvider')->name('loginSocial');
-    Route::get('/auth/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
-
-    /* 
-    *   Pág. inicial 
-    */
-    Route::get('/', 'IndexController@index')->name('landing');
-
-    /* 
-    *   Barra de pesquisa de produtos
-    */
-    Route::get('/find', 'SearchController@find')->name('search');
+    Route::get('/historico/{id}/clean', 'HistoricController@destroy');
 
     /*
-    *   View produto
+    *   Checkout
     */
-    Route::get('/produto/{id}', 'ProductController@show');
-
-    Route::get('/ofertas', 'OfertasController@view')->name('offers');
-
-    /*
-    *   Checar CEP
-    */
-    Route::get('/checkout/address/cep', 'CheckoutController@checkCep')->name('checkCep');
-
-    Route::get('/cupom/validate', 'AdminController@cupomValidate');
-
-    /*
-    *   Calcular CEP
-    */
-    Route::post('/calc/frete', 'CheckoutController@calcFrete')->name('calcFrete');
-
-    Route::group(['middleware' => ['auth']], function () {
-        /*
-        *   User views
-        */
-        Route::get('/perfil', 'UserController@showProfile')->name('perfil');
-        Route::post('/perfil/update', 'UserController@updateProfile');
-        Route::get('/pedidos', 'OrderController@show')->name('orders');
-        Route::get('/historico', 'HistoricController@show')->name('historic');
-
-        Route::post('/avaliar', 'UserController@avaliate')->name('avaliate');
-
-        /*
-        *   Limpar histórico
-        */
-        Route::get('/historico/{id}/clean', 'HistoricController@destroy');
-
-        /*
-        *   Checkout
-        */
-        Route::group(['middleware' => ['Checkout']], function () {
-            Route::get('/checkout/endereco', 'CheckoutController@adressCheckout')->name('adress-check');
-            Route::post('/checkout/address/data', 'CheckoutController@addressData');
-            Route::group(['middleware' => ['CheckoutPayment']], function () {
-                Route::get('/checkout/pagamento', 'CheckoutController@paymentCheckout')->name('payment-check');
-                Route::post('/checkout/payment/data', 'CheckoutController@paymentData');
-                Route::get('/paypal/transaction/complete', 'CheckoutController@paymentSuccess');
-            });
-        });
-
-        /*
-        *   PayPal
-        */
-        Route::post('/create-payment', 'PaymentController@create')->name('create-payment');
-        Route::get('/execute-payment', 'PaymentController@execute');
-        Route::get('/cancel-payment', 'PaymentController@cancel');
-
-        /*
-        *   Order
-        */
-        Route::get('/pedido/cancel', 'OrderController@cancel');
-        Route::get('/pedido/rastrear', 'OrderController@track');
-    });
-
-    /*
-    *   Admin
-    */
-    Route::group(['prefix' => 'admin', 'middleware' => ['admin']], function () {
-        /*
-        *   Landing page admin 
-        */
-        Route::get('/', 'AdminController@index')->name('admin.index');
-
-        /*
-        *   Produtos 
-        */
-        Route::get('/products', 'AdminController@products')->name('admin.products');
-        Route::get('/product/{product_id}/destroy', 'ProductController@destroy');
-        Route::get('/product/{product_id}/edit', 'ProductController@formEdit');
-        Route::post('/product/edit/data', 'ProductController@update')->name('admin.product.update');
-        Route::get('/product/create', 'ProductController@create')->name('admin.createProduct');
-        Route::post('/product/create/data', 'ProductController@store')->name('admin.product.store');
-        Route::get('/product/{product_id}/desconto', 'AdminController@ofertaProdutoView');
-        Route::post('/product/desconto', 'AdminController@ofertaProduto');
-
-        Route::get('/category/desconto', 'AdminController@ofertaCategoriaView');
-        Route::post('/category/desconto/aplicar', 'AdminController@ofertaCategoria');
-
-        Route::get('/cupom', 'AdminController@cupomView')->name('admin.cupons');
-        Route::get('/cupom/{id}/create', 'AdminController@cupomView');
-        Route::get('/cupom/{id}/destroy', 'AdminController@cupomView');
-
-        /*
-        *   Pedidos 
-        */
-        Route::get('/orders', 'AdminController@orders')->name('admin.orders');
-        Route::get('/order/{id}/cancel', 'OrderController@cancel')->name('admin.order.cancel');
-    });
-
-    Route::get('/test-components', function () {
-        return view('test_components');
-    });
-}
-if (App::environment('production')) {
-    # code..
-    Route::group(['prefix' => 'public'], function () {
-        /*
-    *   Cart routes
-    */
-        Route::get('/carrinho/{product_id}/add/', 'CartController@addToCart');
-        Route::get('/carrinho/add/', 'CartController@addToCart')->name('cart.add');
-        Route::get('/carrinho/delete', 'CartController@clearCart')->name('cart.clear');
-        Route::get('/carrinho', 'CartController@show')->name('user.cart');
-        Route::get('/carrinho/{product_id}/remove', 'CartController@removeFromCart');
-        Route::get('/carrinho/{product_rowId}/{qty}/{product_id}', 'CartController@changeQuantity');
-
-        /* 
-    *   Autentificação - Login - Rede Social
-    */
-        Auth::routes();
-        Route::get('/auth/{provider}', 'Auth\LoginController@redirectToProvider')->name('loginSocial');
-        Route::get('/auth/{provider}/callback', 'Auth\LoginController@handleProviderCallback');
-
-        /* 
-    *   Pág. inicial 
-    */
-        Route::get('/', 'IndexController@index')->name('landing');
-
-        /* 
-    *   Barra de pesquisa de produtos
-    */
-        Route::get('/find', 'SearchController@find')->name('search');
-
-        /*
-    *   View produto
-    */
-        Route::get('/produto/{id}', 'ProductController@show');
-
-        Route::get('/ofertas', 'OfertasController@view')->name('offers');
-
-        /*
-    *   Checar CEP
-    */
-        Route::get('/checkout/address/cep', 'CheckoutController@checkCep')->name('checkCep');
-
-        Route::get('/cupom/validate', 'AdminController@cupomValidate');
-
-        /*
-    *   Calcular CEP
-    */
-        Route::post('/calc/frete', 'CheckoutController@calcFrete')->name('calcFrete');
-
-        Route::group(['middleware' => ['auth']], function () {
-            /*
-        *   User views
-        */
-            Route::get('/perfil', 'UserController@showProfile')->name('perfil');
-            Route::post('/perfil/update', 'UserController@updateProfile');
-            Route::get('/pedidos', 'OrderController@show')->name('orders');
-            Route::get('/historico', 'HistoricController@show')->name('historic');
-
-            Route::post('/avaliar', 'UserController@avaliate')->name('avaliate');
-
-            /*
-        *   Limpar histórico
-        */
-            Route::get('/historico/{id}/clean', 'HistoricController@destroy');
-
-            /*
-        *   Checkout
-        */
-            Route::group(['middleware' => ['Checkout']], function () {
-                Route::get('/checkout/endereco', 'CheckoutController@adressCheckout')->name('adress-check');
-                Route::post('/checkout/address/data', 'CheckoutController@addressData');
-                Route::group(['middleware' => ['CheckoutPayment']], function () {
-                    Route::get('/checkout/pagamento', 'CheckoutController@paymentCheckout')->name('payment-check');
-                    Route::post('/checkout/payment/data', 'CheckoutController@paymentData');
-                    Route::get('/paypal/transaction/complete', 'CheckoutController@paymentSuccess');
-                });
-            });
-
-            /*
-        *   PayPal
-        */
-            Route::post('/create-payment', 'PaymentController@create')->name('create-payment');
-            Route::get('/execute-payment', 'PaymentController@execute');
-            Route::get('/cancel-payment', 'PaymentController@cancel');
-
-            /*
-        *   Order
-        */
-            Route::get('/pedido/cancel', 'OrderController@cancel');
-            Route::get('/pedido/rastrear', 'OrderController@track');
-        });
-
-        /*
-    *   Admin
-    */
-        Route::group(['prefix' => 'admin', 'middleware' => ['admin']], function () {
-            /*
-        *   Landing page admin 
-        */
-            Route::get('/', 'AdminController@index')->name('admin.index');
-
-            /*
-        *   Produtos 
-        */
-            Route::get('/products', 'AdminController@products')->name('admin.products');
-            Route::get('/product/{product_id}/destroy', 'ProductController@destroy');
-            Route::get('/product/{product_id}/edit', 'ProductController@formEdit');
-            Route::post('/product/edit/data', 'ProductController@update')->name('admin.product.update');
-            Route::get('/product/create', 'ProductController@create')->name('admin.createProduct');
-            Route::post('/product/create/data', 'ProductController@store')->name('admin.product.store');
-            Route::get('/product/{product_id}/desconto', 'AdminController@ofertaProdutoView');
-            Route::post('/product/desconto', 'AdminController@ofertaProduto');
-
-            Route::get('/category/desconto', 'AdminController@ofertaCategoriaView');
-            Route::post('/category/desconto/aplicar', 'AdminController@ofertaCategoria');
-
-            Route::get('/cupom', 'AdminController@cupomView')->name('admin.cupons');
-            Route::get('/cupom/{id}/create', 'AdminController@cupomView');
-            Route::get('/cupom/{id}/destroy', 'AdminController@cupomView');
-
-            /*
-        *   Pedidos 
-        */
-            Route::get('/orders', 'AdminController@orders')->name('admin.orders');
-            Route::get('/order/{id}/cancel', 'OrderController@cancel')->name('admin.order.cancel');
-        });
-
-        Route::get('/test-components', function () {
-            return view('test_components');
+    Route::group(['middleware' => ['Checkout']], function (){
+        Route::get('/checkout/endereco', 'CheckoutController@adressCheckout')->name('adress-check');
+        Route::post('/checkout/address/data', 'CheckoutController@addressData');
+        Route::group(['middleware' => ['CheckoutPayment']], function (){
+            Route::get('/checkout/pagamento', 'CheckoutController@paymentCheckout')->name('payment-check');
+            Route::post('/checkout/payment/data', 'CheckoutController@paymentData');
+            Route::get('/paypal/transaction/complete', 'CheckoutController@paymentSuccess');
         });
     });
-}
+
+    /*
+    *   PayPal
+    */
+    Route::post('/create-payment', 'PaymentController@create')->name('create-payment');
+    Route::get('/execute-payment', 'PaymentController@execute');
+    Route::get('/cancel-payment', 'PaymentController@cancel');
+
+    /*
+    *   Order
+    */
+    Route::get('/pedido/cancel', 'OrderController@cancel');
+    Route::get('/pedido/rastrear', 'OrderController@track');
+});
+
+/*
+*   Admin
+*/
+Route::group(['prefix' => 'admin', 'middleware' => ['admin']], function (){
+    /*
+    *   Landing page admin 
+    */
+    Route::get('/', 'AdminController@index')->name('admin.index');
+    
+    /*
+    *   Produtos 
+    */
+    Route::get('/products', 'AdminController@products')->name('admin.products');
+    Route::get('/product/{product_id}/destroy', 'ProductController@destroy');
+    Route::get('/product/{product_id}/edit', 'ProductController@formEdit');
+    Route::post('/product/edit/data', 'ProductController@update')->name('admin.product.update');
+    Route::get('/product/create', 'ProductController@create')->name('admin.createProduct');
+    Route::post('/product/create/data', 'ProductController@store')->name('admin.product.store');
+    Route::get('/product/{product_id}/desconto', 'AdminController@ofertaProdutoView');
+    Route::post('/product/desconto', 'AdminController@ofertaProduto');
+    
+    Route::get('/category/desconto', 'AdminController@ofertaCategoriaView');
+    Route::post('/category/desconto/aplicar', 'AdminController@ofertaCategoria');
+    
+    Route::get('/cupom', 'AdminController@cupomView')->name('admin.cupons');
+    Route::get('/cupom/{id}/create', 'AdminController@cupomView');
+    Route::get('/cupom/{id}/destroy', 'AdminController@cupomView');
+
+    /*
+    *   Pedidos 
+    */
+    Route::get('/orders', 'AdminController@orders')->name('admin.orders');
+    Route::get('/order/{id}/cancel', 'OrderController@cancel')->name('admin.order.cancel');
+});
+
+Route::get('/test-components', function(){
+    return view('test_components');
+});
