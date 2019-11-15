146baee9d5ff31e898ae64057676d8bba949192d
Gabriel <gabrieloliveira9669@gmail.com>
2019-11-15 19:18:36 -0300

    seeds produtos

--- resources/views/new_landing.blade.php
+++ resources/views/new_landing.blade.php
@@ -77,7 +77,7 @@
                 <h4 class="font-weight-normal mb-0">
                   R$
                   @php
-                    $formatted_price = number_format($product->price, 2, ',', '');   
+                    $formatted_price = number_format($product->valor, 2, ',', '');   
                     echo $formatted_price;
                   @endphp
                 </h4>
@@ -94,7 +94,7 @@
             </div>
             <div class="mdc-card__actions">
               <div class="mdc-card__action-icons">
-                @if($product->qtd_last == 0)
+                @if($product->qtd_restante == 0)
                 <span class="bg-warning btn">Esgotado</span>
                 @else
                 <form id="comprarAgora-form" action="{{ route('comprarAgora') }}" method="POST" class="d-none">
