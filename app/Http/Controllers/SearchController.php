b1ba8ffa76f4f1863d94d0b07d92b56c66a28aa6
Geovanne Padilha <geovannepadilha@hotmail.com>
2019-11-27 18:09:53 -0300

    fix pesquisa de produto domain

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -8,37 +8,44 @@ use Doomus\Product;
 class SearchController extends Controller
 {
     /**
-    * Search for products method
-    * @param Request $search
-    */
+     * Search for products method
+     * @param Request $search
+     */
     public function find(Request $search)
     {
-        if($search->ajax()){
+        $domain = Request::url();
+        if ($domain == "www.doomus.com.br" || $domain == "doomus.com.br") {
+            $domain = "www.doomus.com.br/public";
+        } else {
+            $domain = "localhost:8000";
+        }
+
+        if ($search->ajax()) {
             $query = $search->get('query');
-            if($query !== ''){
-                $data = Product::where('name', 'like', '%'.$query.'%')
-                    ->orWhere('id', 'like', '%'.$query.'%')
+            if ($query !== '') {
+                $data = Product::where('name', 'like', '%' . $query . '%')
+                    ->orWhere('id', 'like', '%' . $query . '%')
                     ->orderBy('id', 'DESC')
                     ->get();
 
                 $total_qtd = $data->count();
             }
 
-            if($total_qtd > 0){
+            if ($total_qtd > 0) {
                 $output = '';
                 $count = 0;
-                foreach($data as $row){
-                    if($count == 6){
-                        $output .= "<a class='dropdown-item'><span style='margin-left:200px;font-size:14px;margin-top:4px'>Encontrado ".count($data)."</span></a>";
+                foreach ($data as $row) {
+                    if ($count == 6) {
+                        $output .= "<a class='dropdown-item'><span style='margin-left:200px;font-size:14px;margin-top:4px'>Encontrado " . count($data) . "</span></a>";
                         break;
                     }
                     $category = $row->category;
                     $style = "style='margin-top:6px;margin-bottom:6px;'";
-                    $output .= "<a href="."/produto/$row->id"." class='dropdown-item' $style>";
+                    $output .= "<a href=" . $domain . "/produto/$row->id" . " class='dropdown-item' $style>";
                     $output .= "<strong>$row->name</strong><span class='float-right'>$category->name</span></a>";
                     $count++;
                 }
-            }else{
+            } else {
                 $output = '<a class="dropdown-item">Nada encontrado</a>';
             }
 
